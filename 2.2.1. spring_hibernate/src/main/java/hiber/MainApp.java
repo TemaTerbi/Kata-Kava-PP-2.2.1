2074bad7fdb758f6b0a6a72a84fc345594719044
TemaTerbi <tematerbi@mail.ru>
2023-06-22 14:16:51 +0300

    task done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,22 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("Harry", "Potter", "potter@hogwarts.com");
+      User user2 = new User("Hermione", "Granger", "granger@hogwarts.com");
+      User user3 = new User("Ron", "Weasley", "weasley@hogwarts.com");
+      User user4 = new User("Remus", "Lupin", "lupin@hogwarts.com");
+
+      Car car1 = new Car("Lightning", 2021);
+      Car car2 = new Car("Nimbus", 1001);
+      Car car3 = new Car("Cleanliness", 7);
+      Car car4 = new Car("Comet", 290);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +39,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,5 +1,7 @@
 package hiber.model;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import javax.persistence.*;
 
 @Entity
@@ -18,6 +20,9 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+   private Car car;
 
    public User() {}
    
@@ -57,5 +62,14 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
    }
 }
